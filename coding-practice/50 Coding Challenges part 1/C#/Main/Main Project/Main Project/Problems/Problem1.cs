4c612906a68bb8dee1681f4fbfc14d5f3057e740
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-07-27 16:09:17 +0700

    create class Problem1.cs

---
+++ coding-practice/50 Coding Challenges part 1/C#/Main/Main Project/Main Project/Problems/Problem1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Project.Problems
+{
+    public class Problem1
+    {
+        public void Main()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- coding-practice/50 Coding Challenges part 1/C#/Main/Main Project/Main Project/Program.cs
+++ coding-practice/50 Coding Challenges part 1/C#/Main/Main Project/Main Project/Program.cs
@@ -1,7 +1,11 @@
-﻿public class Program
+﻿using Main_Project.Problems;
+
+public class Program
 {
     public static void Main()
     {
         Console.WriteLine("Hello");
+        Problem1 problem1 = new Problem1();
+        problem1.Main();
     }
 }
