6bb51ca2aac12357d5bbe23d24254d850084809d
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-07-28 23:16:14 +0700

    C# CodeSignal/Intro/add solved

---
+++ coding-practice/CodeSignal/C#/CodeSignal/CodeSignal/Intro/Add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSignal.Intro
+{
+    public class Add
+    {
+        public int Solution(int param1,  int param2)
+        {
+            return (param1 + param2);
+        }
+    }
+}
