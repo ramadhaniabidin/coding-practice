245817b9ec5934801b342452f48f8d77b9ba8657
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2024-01-19 18:33:43 +0700

    Update

--- coding-practice/CodeSignal/C#/CodeSignal/CodeSignal/Program.cs
+++ coding-practice/CodeSignal/C#/CodeSignal/CodeSignal/Program.cs
@@ -8,6 +8,69 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("Hello");
+        int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9} };
+        int[] array = { 1, 2, 3 };
+        
+
+        //Console.WriteLine(string.Join(", ", array));
+
+
+        //for (int i = 0; i < matrix.GetLength(0); i++)
+        //{
+        //    //for (int j = 0; j < matrix.GetLength(1); j++)
+        //    //{
+        //    //    if ((i + 1) < matrix.GetLength(0))
+        //    //    {
+        //    //        Console.Write($"{matrix[j, i]}, ");
+        //    //    }
+
+        //    //    else
+        //    //    {
+        //    //        Console.Write($"{matrix[j, i]}\n");
+        //    //    }
+        //    //}
+
+        //    if ((i + 1) < matrix.GetLength(0))
+        //    {
+        //        Console.Write($"{matrix[0, i]}, ");
+        //    }
+
+        //    else
+        //    {
+        //        Console.Write($"{matrix[0, i]}\n");
+        //    }
+
+        //    //j++;
+        //}
+
+
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                if ((i + 1) < matrix.GetLength(0))
+                {
+                    Console.Write($"{matrix[j, i]}, ");
+                }
+
+                else
+                {
+                    Console.Write($"{matrix[j, i]}\n");
+                }
+            }
+
+            //if ((i + 1) < matrix.GetLength(0))
+            //{
+            //    Console.Write($"{matrix[0, i]}, ");
+            //}
+
+            //else
+            //{
+            //    Console.Write($"{matrix[0, i]}\n");
+            //}
+
+            //j++;
+        }
     }
 
 }
---
+++ coding-practice/Daily Coding Problem/Solutions/Python/Arrays.py
@@ -0,0 +1,70 @@
+from functools import reduce
+class Arrays:
+    def __init__(self) -> None:
+        pass
+
+    def HelloWorld(self):
+        print("Hello")
+
+    def SumList(self):
+        myList = [1, 2, 3, 4]
+        print((myList[-1]))
+
+     # This is my answer
+    def ProductList(self):
+        myList = [1, 2, 3, 4]
+        calculatedValue = 1
+        returnedList = []
+        for i in myList:
+            for j in myList:
+                if(i != j):
+                    calculatedValue *= j
+            returnedList.append(calculatedValue)
+            calculatedValue = 1
+
+        returnedList.sort()
+        print(returnedList)
+
+    ## This is the answer from the book
+    def products(self, nums):
+        # Generate prefix products.
+        prefix_products = []
+        for num in nums:
+            if prefix_products:
+                prefix_products.append(prefix_products[-1] * num)
+            else:
+                prefix_products.append(num)
+            # print(prefix_products)
+        
+
+        # Generate suffix products.
+        suffix_products = []
+        for num in reversed(nums):
+            if suffix_products:
+                suffix_products.append(suffix_products[-1] * num)
+            else:
+                suffix_products.append(num)
+            
+        suffix_products = list(reversed(suffix_products))
+        # print(f'Suffix products = {suffix_products}')
+
+
+        # Generate result from the product of prefixes and suffixes
+        result = []
+        for i in range(len(nums)):
+            print(f'i = {i}')
+            if i == 0:
+                result.append(suffix_products[i + 1])
+            elif i == len(nums) - 1:
+                result.append(prefix_products[i - 1])
+            else:
+                result.append(prefix_products[i - 1] * suffix_products[i + 1])
+            
+        print(f'Prefix products = {prefix_products}')
+        print(f'Suffix products = ', suffix_products)
+        print(result)
+    
+
+myObject = Arrays()
+# myObject.ProductList()
+myObject.products([1, 2, 3, 4])
