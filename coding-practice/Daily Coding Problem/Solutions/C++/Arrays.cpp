d4a117aa9dce6f2d44ea9fdb810eb552654fdce2
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2024-01-21 20:57:30 +0700

    Update Arrays.exe and Arrays.cpp

--- coding-practice/Daily Coding Problem/Solutions/C++/Arrays.cpp
+++ coding-practice/Daily Coding Problem/Solutions/C++/Arrays.cpp
@@ -1,25 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Arrays{
+class Arrays
+{
 	string message = "hello";
 	int myArray[3] = {1, 2, 3};
-	
-	public:
-	void hello(){
-		int arrayLength = sizeof(myArray)/sizeof((myArray[0]));
-		cout<<"The length of myArray is: "<<arrayLength<<endl;
+
+public:
+	void hello()
+	{
+		int arrayLength = sizeof(myArray) / sizeof((myArray[0]));
+		cout << "The length of myArray is: " << arrayLength << endl;
 	}
-	
+
+	int *returningArray()
+	{
+		static int outputArray[2] = {1, 2};
+		return outputArray;
+	}
 	// void myProductAnswer(int input[5]){
 	// 	for(i = 0; i < 5; i++){
-			
+
 	// 	}
 	// }
 };
 
-int main(){
+int main()
+{
 	Arrays myObject;
-	myObject.hello();
+	int *myArray = myObject.returningArray();
+	// myObject.hello();
+	cout << "[";
+	for (int i = 0; i < 2; i++)
+	{
+		/* code */
+		if (i != 1)
+		{
+			cout << myArray[i] << ", ";
+		}
+		else
+		{
+			cout << myArray[i] << "";
+		}
+	}
+	cout << "]";
 
 	return 0;
 }
