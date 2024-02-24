fdc3ca9b44693102f1f1c97555dbf058e4ec0c6f
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2024-02-24 11:01:21 +0700

    Update Python Arrays Solution

--- coding-practice/Daily Coding Problem/Solutions/Python/Arrays.py
+++ coding-practice/Daily Coding Problem/Solutions/Python/Arrays.py
@@ -1,4 +1,57 @@
 from functools import reduce
+class MySolution:
+    def __init__(self) -> None:
+        pass
+    def products(self, nums):
+        calculatedValue = 1
+        returnedList = []
+        for i in nums:
+            for j in nums:
+                if(i != j):
+                    calculatedValue *= j
+            returnedList.append(calculatedValue)
+            calculatedValue = 1
+        return returnedList
+    
+class BookSolution:
+    def __init__(self) -> None:
+        pass
+    def products(self, nums):
+        # Generate prefix products.
+        prefix_products = []
+        for num in nums:
+            if prefix_products:
+                prefix_products.append(prefix_products[-1] * num)
+            else:
+                prefix_products.append(num)
+            # print(prefix_products)
+        
+
+        # Generate suffix products.
+        suffix_products = []
+        for num in reversed(nums):
+            if suffix_products:
+                suffix_products.append(suffix_products[-1] * num)
+            else:
+                suffix_products.append(num)
+            
+        suffix_products = list(reversed(suffix_products))
+        # print(f'Suffix products = {suffix_products}')
+
+
+        # Generate result from the product of prefixes and suffixes
+        result = []
+        for i in range(len(nums)):
+            if i == 0:
+                result.append(suffix_products[i + 1])
+            elif i == len(nums) - 1:
+                result.append(prefix_products[i - 1])
+            else:
+                result.append(prefix_products[i - 1] * suffix_products[i + 1])
+        return result
+    
+
+
 class Arrays:
     def __init__(self) -> None:
         pass
@@ -65,10 +118,11 @@ class Arrays:
         return result
     
 nums = [1, 2, 3, 4, 5]
-myObject = Arrays()
+myObject = MySolution()
+bookObject = BookSolution()
 
-myAnswer = myObject.ProductList(nums)
-bookAnswer = myObject.products([1, 2, 3, 4, 5])
+myAnswer = myObject.products(nums)
+bookAnswer = bookObject.products(nums)
 
 print(f'My Answer: {myAnswer}')
 print(f'Book\'s Answer: {bookAnswer}')
