489af119906078fe86bc52f6fb46b8473ead5f3c
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2024-04-30 15:00:39 +0700

    Update Solution.py
    Add solution from CharGPT

--- coding-practice/LeetCode 75/Megre Strings Alternately/Solution.py
+++ coding-practice/LeetCode 75/Megre Strings Alternately/Solution.py
@@ -14,7 +14,24 @@ class Solution:
             i += 1
         return output
     
+class ChatGPTSolution:
+    def __init__(self, word1, word2):
+        self.word1 = word1
+        self.word2 = word2
+
+    def mergeAlternately(self):
+        output = ''
+        i = 0
+        min_length = min(len(word1), len(word2))
+        for i in range(min_length):
+            output += word1[i] + word2[i]
+        
+        output += word1[min_length:] + word2[min_length:]
+        return output
+    
 word1 = 'abcd'
 word2 = 'pq'
 MySolution = Solution(word1, word2)
+Cheat = ChatGPTSolution(word1, word2)
 print(MySolution.mergeAlternately())
+print(Cheat.mergeAlternately())
