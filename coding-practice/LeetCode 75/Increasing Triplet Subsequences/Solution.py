17995d88b1de9c0947676b00773bcdc4682aaeb2
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2024-05-07 14:53:28 +0700

    Update Solution.py
    this solution is from ChatGPT :(

--- coding-practice/LeetCode 75/Increasing Triplet Subsequences/Solution.py
+++ coding-practice/LeetCode 75/Increasing Triplet Subsequences/Solution.py
@@ -1,42 +1,25 @@
-nums = [1, 3, 2, 7, 5]
+class Solution:
+    def __init__(self) -> None:
+        pass
 
-def max_right(nums, index):
-    # this is for generating the maximum value of the remaining list
-    new_nums = [nums[i] for i in range(len(nums)) if i > index]
-    if len(new_nums) != 0:
-        return max(new_nums)
-    else:
-        return 0
-    
-def max_overall(nums, index):
-    new_nums = [nums[i] for i in range(len(nums)) if i >= index]
-    if len(new_nums) != 0:
-        return max(new_nums)
-    else:
-        return 0
-    
-def bigger_value_index(nums, index):
-    bigger_index_list = []
-    for i in range(len(nums)):
-        if index != len(nums) - 1 and nums[i] > nums[index] and i > index:
-            bigger_index_list.append(i)
-        elif index == len(nums) - 1 and nums[i] > nums[i - 1]:
-            bigger_index_list.append('kosong')
-    return bigger_index_list
+    def increasingTripletSubsequence(self, nums):
+        if len(nums) < 3:
+            return False
+        
+        min_so_far = float('inf')
+        second_min_so_far = float('inf')
+
+        for num in nums:
+            if num <= min_so_far:
+                min_so_far = num
+            elif num <= second_min_so_far:
+                second_min_so_far = num
+            else:
+                return True
             
+        return False
+    
 
-def triplet(nums):
-    my_dict = {}
-    for i in range(len(nums)):
-        my_dict[i] = bigger_value_index(nums, i)
-    print(my_dict)
-    # helper = []
-    # for i in range(len(nums)):
-    #     if i != len(nums) - 1:
-    #         if nums[i] < nums[i + 1]:
-    #             helper.append(i)
-    #     elif i == len(nums) - 1 and nums[i] > nums[i - 1]:
-    #         helper.append(i)
-    # print(helper)
-
-triplet(nums)
+solution = Solution()
+nums = [1, 6, 2, 5, 6]
+print(solution.increasingTripletSubsequence(nums))
