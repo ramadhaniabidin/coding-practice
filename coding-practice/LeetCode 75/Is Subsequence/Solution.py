e559b153c7d578337883505e9e7ac2130972ee84
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2024-05-09 22:18:42 +0700

    create solution for Is Subsequence problem

---
+++ coding-practice/LeetCode 75/Is Subsequence/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+        
+        s_index = 0
+        for char in t:
+            if char == s[s_index]:
+                s_index += 1
+                if s_index == len(s):
+                    return True
+        return False
