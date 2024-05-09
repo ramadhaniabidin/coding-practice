83b866ce295780ce34875adfc01bc7c457952e82
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2024-05-09 14:24:27 +0700

    Improve the Solution of String Compression

--- coding-practice/LeetCode 75/String Compression/Solution.py
+++ coding-practice/LeetCode 75/String Compression/Solution.py
@@ -5,25 +5,23 @@ class Solution:
     def compress(self, chars=["a","a","b","b","c","c","c"]):
         curr_element = chars[0]
         n_occurr = 0
-        str_compress = ""
+        str_compress = []
 
         for i in range(len(chars)):
             if curr_element == chars[i]:
                 n_occurr += 1
             else:
                 # out.append(f'{curr_element}{n_occurr}')
-                str_compress += f'{curr_element}{n_occurr}' if n_occurr > 1 else f'{curr_element}'
+                str_compress.append(f'{curr_element}{n_occurr}' if n_occurr > 1 else f'{curr_element}')
                 curr_element = chars[i]
                 n_occurr = 1
 
             if i + 1 == len(chars):
                 # out.append(f'{curr_element}{n_occurr}')
-                str_compress += f'{curr_element}{n_occurr}' if n_occurr > 1 else f'{curr_element}'
-        print(str_compress)
+                str_compress.append(f'{curr_element}{n_occurr}' if n_occurr > 1 else f'{curr_element}')
 
         chars.clear()
-        for i in str_compress:
-            chars.append(i)
+        chars.extend(''.join(str_compress))
         return len(chars)
     
 mySolution = Solution()
