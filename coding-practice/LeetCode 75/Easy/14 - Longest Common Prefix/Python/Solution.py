478d4d87c3b2712149afc9a685c6b1f1c8d672a2
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2024-08-17 23:58:04 +0700

    Update Solution.py

--- coding-practice/LeetCode 75/Easy/14 - Longest Common Prefix/Python/Solution.py
+++ coding-practice/LeetCode 75/Easy/14 - Longest Common Prefix/Python/Solution.py
@@ -22,6 +22,33 @@ class Solution:
         # return prefix
         return prefix
     
+    def display_solution(self):
+        loop = True
+
+        while loop:
+            print("Longest Common Prefix")
+            print("This function will search for the longest common prefix of a list of strings")
+            input_string = input("Enter a list of strings separated by commas: ")
+            words = input_string.split(",")
+            longestPrefix = self.longestCommonPrefix(words)
+            print(f'Longest common prefix of {words} is \'{longestPrefix}\'')
+            while True:
+                print("Would you like to repeat? (y/n)")
+                response = input()
+                response = "" if response is None else response
+
+                if response == "":
+                    print("Please choose the option")
+                elif response == "y" or response == "Y":
+                    loop = True
+                    break
+                elif response == "n" or response == "N":
+                    loop = False
+                    break
+                else:
+                    print("Invalid input. Please enter 'y' or 'n'")
+
+    
 obj = Solution()
-longestPrefix = obj.longestCommonPrefix(strs)
-print(f'Longest common prefix of {strs} is \'{longestPrefix}\'')
+obj.display_solution()
+
