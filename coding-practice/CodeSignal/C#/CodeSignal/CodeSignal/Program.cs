89eb139db0d2563a592bd23dd4bad563fbb4d910
ramadhaniabidin <93337270+ramadhaniabidin@users.noreply.github.com>
2024-08-17 13:11:02 +0700

    Update Program.cs

--- coding-practice/CodeSignal/C#/CodeSignal/CodeSignal/Program.cs
+++ coding-practice/CodeSignal/C#/CodeSignal/CodeSignal/Program.cs
@@ -10,10 +10,7 @@ public class Program
     public static void Main()
     {
         Problem13 RomanToInt = new();
-        string romanString = "MCMXCIV";
-        int romanNumber = RomanToInt.romanToInt(romanString);
-
-        Console.WriteLine($"The integer value of {romanString} is {romanNumber}");
+        RomanToInt.DisplaySolution();
     }
 
 }
